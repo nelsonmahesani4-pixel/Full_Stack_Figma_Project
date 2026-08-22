1d3b582efcd638ca63b7aca2b903ad572e96f602
nelsonmahesani <nelsonmahesani4@gmail.com>
2026-08-22 10:12:19 +0500

    made changes in BrowsDressStyles.jsx

--- client/src/components/BrowsDressStyles.jsx
+++ client/src/components/BrowsDressStyles.jsx
@@ -2,46 +2,55 @@ import { Link } from "react-router-dom";
 
 const DRESS_STYLES = [
   {
-    name:"casual",
-    size: "large",
-    image:"/Images/casual.png"
+    name: "Casual",
+    slug: "casual",
+    image: "/Images/casual.png",
   },
   {
     name: "Formal",
-    size: "small",
+    slug: "formal",
     image: "/Images/formal1.png",
   },
   {
     name: "Party",
-    size: "small",
+    slug: "party",
     image: "/Images/party.png",
   },
   {
-    // name: "Gym",
-    size: "large",
-    image:"/Images/gym.png"
-    
+    name: "Gym",
+    slug: "gym",
+    image: "/Images/gym.png",
   },
 ];
 
 export default function BrowseStyles() {
   return (
-    <section className="section dress-style">
+    <section className="dress-style">
       <div className="dress-style__panel">
-        <h2 className="section__title">BROWSE BY DRESS STYLE</h2>
+
+        <h2 className="dress-style__title">
+          BROWSE BY DRESS STYLE
+        </h2>
 
         <div className="dress-style__grid">
+
           {DRESS_STYLES.map((style) => (
             <Link
-  key={style.name}
-  to={`/category?dressStyle=${style.name}`}
-  className={`dress-style__card dress-style__card--${style.size}`}
->
-  <img src={style.image} alt={style.name} />
-  <span>{style.name}</span>
-</Link>
+              key={style.slug}
+              to={`/category?dressStyle=${style.slug}`}
+              className={`dress-style__card dress-style__card--${style.slug}`}
+            >
+              <img
+                src={style.image}
+                alt={style.name}
+              />
+
+              <span>{style.name}</span>
+            </Link>
           ))}
+
         </div>
+
       </div>
     </section>
   );
